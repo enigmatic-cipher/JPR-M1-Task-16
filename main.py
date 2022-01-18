bce2eaa8c7d205536c9e40fe894629c26f0102e6
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-18 15:02:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Given a 4 digit number n as input, find the sum of its digits. Note that n%10 gives its last digit, whereas n/10 removes the last digit. So if n=2456, n%10 is 6 and n/10 is 245. So, run a for loop from 1 to 4 and in the loop do 2 things – calculate the last digit and then remove the last digit.
+Input-> n=2456
+Output-> 17
+"""
+
+n = 2456
+ln = len(str(n))
+total = 0
+for i in range(1,ln+1):
+  
+
